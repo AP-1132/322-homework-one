89463f32fa3ea48403ebeb5df9a6fc79ef196226
Alonso Palmero <palmeroa@iu.edu>
2025-08-27 17:02:02 -0400

    first commit

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -11,7 +11,7 @@ import { IconSymbol } from '@/components/ui/IconSymbol';
 export default function TabTwoScreen() {
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
+      headerBackgroundColor={{ light: '#ae0000ff', dark: '#0fc8c8ff' }}
       headerImage={
         <IconSymbol
           size={310}
